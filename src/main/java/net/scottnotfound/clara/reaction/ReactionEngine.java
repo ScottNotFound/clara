d2ba7504168f1c838536aa5a4f2122e5aef05750
Scott Thiel <scott@scottnotfound.net>
2018-04-02 02:32:50 -0400

    instances of the engine and module

--- src/main/java/net/scottnotfound/clara/reaction/ReactionEngine.java
+++ src/main/java/net/scottnotfound/clara/reaction/ReactionEngine.java
@@ -2,4 +2,20 @@ package net.scottnotfound.clara.reaction;
 
 public class ReactionEngine {
 
+    private static ReactionEngine RE_INSTANCE;
+    private static ReactionBuilder RB_INSTANCE;
+
+    public static ReactionEngine getInstance() {
+        if (RE_INSTANCE == null) {
+            RE_INSTANCE = new ReactionEngine();
+        }
+        return RE_INSTANCE;
+    }
+
+    private ReactionEngine() {
+        RB_INSTANCE = ReactionBuilder.getInstance();
+    }
+
+
+
 }
--- src/main/java/net/scottnotfound/clara/reaction/ReactionModule.java
+++ src/main/java/net/scottnotfound/clara/reaction/ReactionModule.java
@@ -6,6 +6,20 @@ import net.scottnotfound.clara.lang.ICommandReceiver;
 
 public class ReactionModule extends ModuleBase implements ICommandReceiver, IModule {
 
+    private static ReactionModule RM_INSTANCE;
+    private static ReactionEngine RE_INSTANCE;
+
+    public static ReactionModule getInstance() {
+        if (RM_INSTANCE == null) {
+            RM_INSTANCE = new ReactionModule();
+        }
+        return RM_INSTANCE;
+    }
+
+    private ReactionModule() {
+        RE_INSTANCE = ReactionEngine.getInstance();
+    }
+
 
     @Override
     public void executeCommand() {
