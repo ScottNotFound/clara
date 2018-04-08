381d9b95cefd2dfced4646b8e9a151de3a3e6b2f
Scott Thiel <scott@scottnotfound.net>
2018-04-08 01:54:53 -0400

    compute hybridization

--- src/main/java/net/scottnotfound/clara/reaction/ReactionBuilder.java
+++ src/main/java/net/scottnotfound/clara/reaction/ReactionBuilder.java
@@ -2,9 +2,9 @@ package net.scottnotfound.clara.reaction;
 
 import org.openscience.cdk.AtomContainerSet;
 import org.openscience.cdk.Reaction;
-import org.openscience.cdk.interfaces.IAtomContainer;
-import org.openscience.cdk.interfaces.IAtomContainerSet;
-import org.openscience.cdk.interfaces.IReaction;
+import org.openscience.cdk.interfaces.*;
+import org.openscience.cdk.qsar.DescriptorValue;
+import org.openscience.cdk.qsar.descriptors.atomic.AtomHybridizationVSEPRDescriptor;
 import org.openscience.cdk.silent.SilentChemObjectBuilder;
 import org.openscience.cdk.smiles.SmilesParser;
 import uk.ac.cam.ch.wwmm.opsin.NameToStructure;
@@ -70,5 +70,42 @@ class ReactionBuilder {
             }
         }
         return atomContainerSet;
+    }
+
+    private IReaction computeProperties(IReaction reaction) {
+
+        return reaction;
+    }
+
+    private IReaction computeHybridization(IReaction reaction) {
+
+        AtomHybridizationVSEPRDescriptor hybridizationVSEPRDescriptor = new AtomHybridizationVSEPRDescriptor();
+
+        for (IAtomContainer mol : reaction.getReactants().atomContainers()) {
+
+            for (IAtom atom : mol.atoms()) {
+
+                IAtomType.Hybridization hybridization = null;
+
+                DescriptorValue descriptorValue = hybridizationVSEPRDescriptor.calculate(atom, mol);
+                Integer value = Integer.parseInt(descriptorValue.getValue().toString());
+
+                switch (value) {
+                    case 1: hybridization = IAtomType.Hybridization.SP1;    break;
+                    case 2: hybridization = IAtomType.Hybridization.SP2;    break;
+                    case 3: hybridization = IAtomType.Hybridization.SP3;    break;
+                    case 4: hybridization = IAtomType.Hybridization.SP3D1;  break;
+                    case 5: hybridization = IAtomType.Hybridization.SP3D2;  break;
+                    case 6: hybridization = IAtomType.Hybridization.SP3D3;  break;
+                    case 7: hybridization = IAtomType.Hybridization.SP3D4;  break;
+                    case 8: hybridization = IAtomType.Hybridization.SP3D5;  break;
+                }
+
+                atom.setHybridization(hybridization);
+
+            }
+        }
+
+        return reaction;
     }
 }
