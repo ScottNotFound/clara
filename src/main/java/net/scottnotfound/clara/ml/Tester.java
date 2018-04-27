b003b307f415e30654ba7991869c959a3999bf75
Scott Thiel <scott@scottnotfound.net>
2018-04-26 22:33:23 -0400

    added Tester and Trainer files

---
+++ src/main/java/net/scottnotfound/clara/ml/Tester.java
@@ -0,0 +1,10 @@
+package net.scottnotfound.clara.ml;
+
+public class Tester {
+
+
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ src/main/java/net/scottnotfound/clara/ml/Trainer.java
@@ -0,0 +1,10 @@
+package net.scottnotfound.clara.ml;
+
+public class Trainer {
+
+
+    public static void main(String[] args) {
+
+    }
+
+}
